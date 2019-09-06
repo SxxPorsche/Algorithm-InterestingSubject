3e8ccec12e9f97a6749faf6d991f774955cdad6f
sx <tanjiexiong@tungee.com>
2019-09-07 00:43:02 +0800

    选择排序

---
+++ sort/select-sort.js
@@ -0,0 +1,17 @@
+// 选择排序
+function SelectSort(array) {
+  for(let i = 0; i < array.length; i++) {
+    for(let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[i]) {
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+  }
+}
+
+var arr = [5,4,3,2,1];
+SelectSort(arr);
+console.log(arr);
+
+// 时间复杂度：O(n^2)
+// 空间复杂度：O(1)
