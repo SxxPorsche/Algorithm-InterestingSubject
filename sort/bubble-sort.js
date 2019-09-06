001c967b95015c237a4abef923302635f87023e4
sx <tanjiexiong@tungee.com>
2019-09-06 23:10:35 +0800

    排序算法

---
+++ sort/bubble-sort.js
@@ -0,0 +1,17 @@
+// 冒泡排序
+function BubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j+1]] = [array[j+1], array[j]];
+      }
+    }
+  }
+}
+
+var arr = [1, 4, 3, 5, 2];
+BubbleSort(arr);
+console.log(arr);
+
+// 时间复杂度：O(n^2)
+// 空间复杂度：O(1)
