c923e3d2be981b6212cb5d2cf2d766001a22205a
sx <tanjiexiong@tungee.com>
2020-01-14 18:09:55 +0800

    大文件分割上传

---
+++ js/xhrRequest.js
@@ -0,0 +1,23 @@
+function request({
+  url,
+  method = "post",
+  data,
+  headers,
+  requestList,
+}) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    Object.keys(headers).forEach(key =>
+      xhr.setRequestHeader(key, headers[key])
+    );
+    xhr.send(data);
+    xhr.onload = (e) => {
+      resolve({
+        data: e.target.response,
+      });
+    }
+  });
+}
+
+module.exports = request;
